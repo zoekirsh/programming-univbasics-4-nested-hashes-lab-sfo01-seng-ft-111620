b77cbeda4ff1adcb19146e09b6c0b5db653978e9
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-14 18:16:22 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    
+p programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+p programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+p programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +82,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
 
+p programmer_hash
 end
 
 def changing_alan
@@ -103,7 +109,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+alans_new_info = "GUI"
+programmer_hash[:alan_kay][:known_for] = alans_new_info
 
+p programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +134,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+p programmer_hash
 end
